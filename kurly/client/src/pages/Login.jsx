35656881e982e771238292819a61a43ce816e5ca
ssussu <ssussu00623@gmail.com>
2025-02-05 18:24:08 +0900

    ==2025.02.05(end)

---
+++ kurly/client/src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div /** */ > 
+            <form action=""> 
+                <ul>
+                    <li>
+                        <div>
+                            <input type="text" 
+                                placeholder='아이디를 입력해주세요'/>
+                            <input type="text" 
+                                placeholder='비밀번호를 입력해주세요'/>
+                        </div>
+                    </li>
+                </ul> 
+                <ul>
+                    <li><a href="#">아이디찾기</a></li>
+                    <li><a href="#">비밀번호찾기</a></li>
+                </ul> 
+                <div>
+                    <button type='button'>로그인</button>
+                    <button><Link></Link></button> 
+                </div>
+            </form>
+        </div>
+    );
+}
+
